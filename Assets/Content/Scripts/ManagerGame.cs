b45b948391ef19dba6ec156027e36e411ec0aca9
F286 <freedebreuil@gmail.com>
2017-07-10 18:53:11 +0900

    setting up stack

---
+++ Assets/Content/Scripts/ManagerGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerGame : MonoBehaviour {
+    public Game game;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
