2f1454436ebff734c250ddcef0a71aa794692477
Free Debreuil <freedebreuil@gmail.com>
2017-07-23 23:52:14 +0900

    begin and end turn events working

--- Assets/Content/Scripts/Gameplay/TRIGGER_OnTurn.cs
+++ Assets/Content/Scripts/Gameplay/TRIGGER_OnTurn.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +7,24 @@ public enum TurnState {
 	Begin,
 	End,
 }
-public class TRIGGER_OnTurn : MonoBehaviour, ITrigger {
+public class TRIGGER_OnTurn : MonoBehaviour, ITrigger, IOnGlobalEvent {
 	public PlayerType player = PlayerType.Friendly;
 	public TurnState turnState;
 
+	public GlobalEventName globalEventName {
+		get {
+			return GlobalEventName.Any;
+		}
+	}
+
+	public void OnGlobalEvent(GlobalEventName eventName) {
+		if (turnState == TurnState.Begin && eventName == GlobalEventName.BeginTurn ||
+		    turnState == TurnState.End && eventName == GlobalEventName.EndTurn) {
+			if (this.GetLocation() == CardLocation.Board) {
+				if (this.GetPlayer() == ManagerGame.instance.currentPlayer) {
+					this.TriggerEvent();
+				}
+			}
+		}
+	}
 }
