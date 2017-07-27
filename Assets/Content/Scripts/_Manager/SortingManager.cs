e0739c16df1e3f01d9c96ecbfa24ebf61dcea8e2
Free Debreuil <freedebreuil@gmail.com>
2017-07-27 17:21:29 +0900

    added sorting of sprite groups

---
+++ Assets/Content/Scripts/_Manager/SortingManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SortingManager : MonoBehaviour {
+	public void LateUpdate() {
+		int index = 1;
+		foreach (var item in GetComponentsInChildren<SortingGroup>()) {
+			item.sortingOrder = -index;
+			item.transform.position = new Vector3(item.transform.position.x, item.transform.position.y, 0.1f * index);
+			index += 1;
+		}
+	}
+}
