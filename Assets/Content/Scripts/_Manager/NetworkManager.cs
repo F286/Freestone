ca3e065be58dbdcfd06dffa4f77b2e92246912c3
Free Debreuil <freedebreuil@gmail.com>
2017-07-30 17:40:49 +0900

    working on networked commands

---
+++ Assets/Content/Scripts/_Manager/NetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkManager : NetworkBehaviour {
+	public void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			CmdSpawn();
+		}
+	}
+
+	[Command]
+	void CmdSpawn() {
+		var load = Resources.Load<NETWORK_EndTurn>("Network End Turn");
+		var create = Instantiate(load, transform);
+		create.test = "hello 123";
+		NetworkServer.Spawn(create.gameObject);
+	}
+}
