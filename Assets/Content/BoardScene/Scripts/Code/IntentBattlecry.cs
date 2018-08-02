4d34b2ce0347089ec404e19358cdf8b4a7769b28
Free Debreuil <freedebreuil@gmail.com>
2018-08-02 22:48:26 +0900

    working on card code

--- Assets/Content/BoardScene/Scripts/Code/IntentBattlecry.cs
+++ Assets/Content/BoardScene/Scripts/Code/IntentBattlecry.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragToBoard : MonoBehaviour {
+public class IntentBattlecry : MonoBehaviour {
 
-	public CardEvent onDragToBoard;
+	public CardEvent onSelectTarget;
 
 }
