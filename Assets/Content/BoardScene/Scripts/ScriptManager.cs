06f8b7653cc677a8c89f1521fe0d17ec9d2e7dde
Free Debreuil <freedebreuil@gmail.com>
2018-08-02 19:01:34 +0900

    working on card execute behaviour

---
+++ Assets/Content/BoardScene/Scripts/ScriptManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonSharp.Interpreter;
+
+public class ScriptManager : MonoBehaviour {
+
+	public void Start () {
+		// print(MoonSharpFactorial());
+		MoonSharpFactorial();
+	}
+	
+	void MoonSharpFactorial() {
+		string script = @"    
+			-- defines a factorial function
+			function fact (n)
+				if (n == 0) then
+					return 1
+				else
+					return n*fact(n - 1)
+				end
+			end
+
+			squares = {1, 4, 9, 16, 25, 36, 49, 64, 81}
+			return squares";
+
+		DynValue res = Script.RunString(script);
+		print(res.String);
+		// return res.Number;
+	}
+
+}
